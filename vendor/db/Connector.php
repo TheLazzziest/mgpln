bef87259ff3af5b630f573c886271b6bc7a82a58
TheLazzziest <mxyakovenko9@gmail.com>
2016-01-29 19:19:04 +0300

    db connector

--- vendor/db/Connector.php
+++ vendor/db/Connector.php
@@ -1,31 +1,32 @@
 <?php
 namespace Vendor\Db;
 
+use PDO;
+
 abstract class Connector implements Interfaces\ConnectorInterface
 {
     //MIN number of db parameters to establish the connection for mysql db
     const MIN_PARAMS = 4;
 
     //If you are going to use generic wp db handle
-    private $link;
+    private $_link;
     //
 
     //If you are going to establish the separate connection to your wp
-    private $dbhost;
-    private $dbname;
-    private $dbuser;
-    private $dbpassword;
-    //
+    private $_dbhost;
+    private $_dbname;
+    private $_dbuser;
+    private $_dbpassword;
 
-    protected $prefix;
-    protected $charset;
-    protected $collate;
+    protected $_prefix;
+    protected $_charset;
+    protected $_collate;
 
     public function __construct()
     {
         require_once dirname(__FILE__)."/Settings.php";
         if(empty($config) || count($config) < MIN_PARAMS )
-            throw new WP_Error('missing', "PLugin db error. Missing db configuration set");
+            throw new \Exception("PLugin db error. Missing db configuration set");
         $this->initProps($config);
     }
 
@@ -33,6 +34,7 @@ abstract class Connector implements Interfaces\ConnectorInterface
         foreach($config as $key => $value){
                 if(property_exists($this,$key))
                     $this->$key = $value;
+                throw new \Exception("Missing {$key} property");
         }
     }
 
@@ -41,26 +43,26 @@ abstract class Connector implements Interfaces\ConnectorInterface
      */
     public function connect()
     {
-        if(empty($this->link)){
+        if(empty($this->_link)){
             try{
-                $this->link = new PDO("mysql:host=$this->dbhost;dbname=$this->dbname", $this->dbuser,$this->dbpassword);
-            }catch(PDOException $e){
-               throw new WP_Error($e->getCode(), $e->getMessage());
+                $this->_link = new PDO("mysql:host=$this->_dbhost;dbname=$this->_dbname", $this->_dbuser,$this->_dbpassword);
+            }catch(\PDOException $e){
+               throw new \Exception($e->getMessage(),$e->getCode());
             }
         }
-        return !empty($this->link); // if the link does exist
+        return !empty($this->_link); // if the link does exist
     }
 
     public function disconnect()
     {
-        if(!empty($this->link))
+        if(!empty($this->_link))
             $this->link = null;
-        return empty($this->link);
+        return empty($this->_link);
     }
 
     public function get_link()
     {
-        return !empty($this->link) ? $this->link : null;
+        return !empty($this->_link) ? $this->_link : null;
     }
 }
 ?>
