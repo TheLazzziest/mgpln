b84a2d0edbbef6895c3eb70d02e80bf43a9f5d4d
TheLazzziest <mxyakovenko9@gmail.com>
2016-04-12 15:29:46 +0300

    Base exception class for whole plugin

---
+++ Vendor/Exceptions/MegaformsException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 11.02.16
+ * Time: 18:57
+ */
+
+namespace Megaforms\Vendor\Exceptions;
+
+
+class MegaformsException extends \Exception
+{
+
+}
