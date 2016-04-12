86db400cdb10ef2f97a8afa49e82ab86373c1349
TheLazzziest <mxyakovenko9@gmail.com>
2016-04-12 15:24:14 +0300

    Class for Db debugging

---
+++ Vendor/Db/Query/DebugStmnt.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 15.02.16
+ * Time: 14:46
+ */
+
+namespace Megaforms\Vendor\Db\Query;
+
+
+abstract class DebugStmnt extends QueryBase
+{
+
+}
