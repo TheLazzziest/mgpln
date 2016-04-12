e70d66907fa3cec6826d30887b44003245542cf1
TheLazzziest <mxyakovenko9@gmail.com>
2016-04-12 15:24:33 +0300

    Class for making join queries

---
+++ Vendor/Db/Query/JoinStmnt.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 15.02.16
+ * Time: 0:20
+ */
+
+namespace Megaforms\Vendor\Db\Query;
+
+
+trait JoinStmnt
+{
+    protected function join(){
+
+    }
+
+    protected function innerJoin(){
+
+    }
+
+    protected function leftJoin(){
+
+    }
+
+    protected function rightJoin(){
+
+    }
+}
