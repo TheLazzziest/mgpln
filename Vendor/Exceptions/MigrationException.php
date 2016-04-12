8779de98cdaf825b075e82ee0277dee29a7c1801
TheLazzziest <mxyakovenko9@gmail.com>
2016-04-12 15:30:01 +0300

    Class for DB migrations

---
+++ Vendor/Exceptions/MigrationException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 21.02.16
+ * Time: 9:49
+ */
+
+namespace Megaforms\Vendor\Exceptions;
+
+
+class MigrationException extends MegaformsException
+{
+
+}
