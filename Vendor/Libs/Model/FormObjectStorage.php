c57338b6e25a1f0cccff4df2f799976055faa1f9
TheLazzziest <mxyakovenko9@gmail.com>
2016-04-12 15:22:59 +0300

    Abstract class for form object storing

---
+++ Vendor/Libs/Model/FormObjectStorage.php
@@ -0,0 +1,17 @@
+<?php
+namespace Megaforms\Vendor\Libs\Model;
+use SplObjectStorage;
+use SplObserver;
+use SplSubject;
+
+
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 26.03.16
+ * Time: 10:52
+ */
+abstract class FormObjectStorage extends SplObjectStorage implements SplObserver
+{
+
+}
