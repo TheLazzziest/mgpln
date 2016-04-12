16e9323f0fb4fe612259024819470cd4472f9281
TheLazzziest <mxyakovenko9@gmail.com>
2016-04-12 15:32:11 +0300

    Controller interface

---
+++ Vendor/Libs/Controller/ControllerInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 10.04.16
+ * Time: 5:54
+ */
+
+namespace Megaforms\Vendor\Libs\Controller;
+
+
+interface ControllerInterface
+{
+    public function index();
+}
