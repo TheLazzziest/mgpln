589624edc73eefe4ea286720f6532de7b6ade59e
TheLazzziest <mxyakovenko9@gmail.com>
2016-04-12 15:28:22 +0300

    Exception for DB adapter class

---
+++ Vendor/Exceptions/AdapterException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 11.02.16
+ * Time: 18:49
+ */
+
+namespace Megaforms\Vendor\Exceptions;
+
+
+class AdapterException extends MegaformsException
+{
+
+}
