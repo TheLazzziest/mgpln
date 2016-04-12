4990a50b0c0616777960bdd47bd8ecdd321ef781
TheLazzziest <mxyakovenko9@gmail.com>
2016-04-12 15:19:30 +0300

    Conditional statements

---
+++ Vendor/Db/Query/ConditionalStmnt.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 15.02.16
+ * Time: 0:17
+ */
+
+namespace Megaforms\Vendor\Db\Query;
+
+
+trait ConditionalStmnt
+{
+    protected function distinct(){
+
+    }
+
+    protected function where($args){
+
+    }
+
+    protected function like($pattern){
+
+    }
+
+    protected function having($args){
+
+    }
+
+    protected function limit(){
+
+    }
+
+    protected function top(){
+
+    }
+
+    protected function orderBy($columns){
+
+    }
+
+    protected function desc(){
+
+    }
+
+    protected function asc(){
+
+    }
+}
