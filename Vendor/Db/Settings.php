e0a4d8d861bb3ab0c191c44a7389b00604001505
TheLazzziest <mxyakovenko9@gmail.com>
2016-04-12 15:25:49 +0300

    WP api wrapper

--- Vendor/Db/Settings.php
+++ Vendor/Db/Settings.php
@@ -2,11 +2,12 @@
 global $wpdb;
 
 $config = [
-    'link' => (!empty($wpdb)) ? $wpdb : '',
-    'dbhost' => defined("DB_HOST") ? DB_HOST : '',
-    'dbname' => defined("DB_NAME") ? DB_NAME : '',
-    'dbuser' => defined("DB_USER") ? DB_USER : '',
-    'dbpassword' => defined("DB_PASSWORD") ? DB_PASSWORD : '',
+    '_link' => (!empty($wpdb)) ? $wpdb : '',
+    '_driver' => defined("DB_DRIVER") ? DB_DRIVER : 'mysql',
+    '_dbhost' => defined("DB_HOST") ? DB_HOST : '',
+    '_dbname' => defined("DB_NAME") ? DB_NAME : '',
+    '_dbuser' => defined("DB_USER") ? DB_USER : '',
+    '_dbpassword' => defined("DB_PASSWORD") ? DB_PASSWORD : '',
     'charset' => (!empty($wpdb->charset)) ? $wpdb->charset : '',
     'collate' => (!empty($wpdb->collate)) ? $wpdb->collate : '',
     'prefix' => (!empty($wpdb->prefix)) ? $wpdb->prefix : '',
