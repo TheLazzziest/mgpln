6bc05e39859aff3ce633ddf23f18d621c3bc9843
TheLazzziest <mxyakovenko9@gmail.com>
2016-04-12 15:15:03 +0300

    Main file for internationalization in wp

--- Vendor/Core/I18n.php
+++ Vendor/Core/I18n.php
@@ -1,16 +1,31 @@
 <?php
 namespace Megaforms\Vendor\Core;
-    class I18n{
+
+use Megaforms\Vendor\Libs\Helpers\CommonHelpers;
+
+defined("MEGAFORMS_BOOTSTRAPPED") or die("I'm only the wp plugin");
+final class I18n{
         /**
          * The domain specified for this plugin.
          *
          * @access   private
          * @var      string    $domain    The domain identifier for this plugin.
          */
-        private $domain;
+        private $_domain;
 
+        public function __construct($domain){
+            if( is_string($domain) === true
+                && empty(CommonHelpers::strim($domain)) === false ){
+
+                $this->_domain = $domain;
+            }
+        }
         public function load_plugin_textdomain(){
-            load_plugin_textdomain($this->domain,false,plugin_dir_path(dirname(__FILE__)));
+            load_plugin_textdomain(
+                $this->_domain,
+                false,
+                plugin_dir_path(dirname(__FILE__))
+            );
         }
     }
 ?>
