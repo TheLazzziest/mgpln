4dd1e612a9a516694b6bff00401676a405f43f99
TheLazzziest <mxyakovenko9@gmail.com>
2016-04-12 15:19:03 +0300

    Base CRUD operations

---
+++ Vendor/Db/Query/CRUDBase.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 15.02.16
+ * Time: 0:09
+ */
+
+namespace Megaforms\Vendor\Db\Query;
+
+
+abstract class CRUDBase extends QueryBase
+{
+    protected function __construct(){
+        parent::__construct();
+    }
+
+    protected function select($params = '*'){
+
+    }
+
+    protected function insert($params){
+
+    }
+
+    protected function replace($params){
+
+    }
+
+    protected function update($params){
+
+    }
+
+    protected function delete($params){
+
+    }
+}
