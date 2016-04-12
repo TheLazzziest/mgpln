254401d9aab728b798b0b3f70357ec420a05e947
TheLazzziest <mxyakovenko9@gmail.com>
2016-04-12 15:34:30 +0300

    Base class for assets

---
+++ Vendor/Libs/View/AssetManager.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 09.04.16
+ * Time: 7:25
+ */
+
+namespace Megaforms\Vendor\Libs\View;
+
+
+abstract class AssetManager
+{
+
+    abstract public function register_js();
+
+    abstract public function register_css();
+
+}
