f7d4a44104d17f9885b83accfd139f8ee932d0a6
TheLazzziest <mxyakovenko9@gmail.com>
2016-04-12 15:28:57 +0300

    Class for Db query errros

---
+++ Vendor/Exceptions/DbException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 21.02.16
+ * Time: 9:48
+ */
+
+namespace Megaforms\Vendor\Exceptions;
+
+
+class DbException extends MegaformsException
+{
+
+}
