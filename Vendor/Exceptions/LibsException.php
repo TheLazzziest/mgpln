516631e5ec862a979e499a940b6659ff08840947
TheLazzziest <mxyakovenko9@gmail.com>
2016-04-12 15:29:29 +0300

    Exception concerning Library classes

---
+++ Vendor/Exceptions/LibsException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 26.03.16
+ * Time: 11:05
+ */
+
+namespace Megaforms\Vendor\Exceptions;
+
+
+class LibsException extends MegaformsException
+{
+
+}
