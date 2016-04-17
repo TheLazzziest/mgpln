c61fa4106f88b909b12d7e79816e660c2307acd5
TheLazzziest <mxyakovenko9@gmail.com>
2016-04-17 10:22:59 +0300

    performing uninstalling of Megaforms plugin

--- uninstall.php
+++ uninstall.php
@@ -1,15 +1,19 @@
 <?php
 
+use Megaforms\Vendor\Core\PluginMigration;
+
 if(!defined('WP_UNINSTALL_PLUGIN')){
     exit();
 }
+
+require_once('megaforms.php');
+spl_autoload_register('MegaformsBootstrap::autoload');
+
 try{
-
-    $migrator = new Megaforms\Vendor\Db\PluginMigration();
-
-    $migrator->down();
+	/** @method PluginMigration down */
+    PluginMigration::load()->down();
 
 }catch(\Megaforms\Vendor\Exceptions\MegaformsException $error){
-    Megaforms\Vendor\Libs\Helpers\CommonHelpers::handle_exception($error->getMessage(), $error->getCode());
+    Megaforms\Vendor\Libs\Helpers\CommonHelpers::handle_exception($error);
 }
 ?>
